79fd501cd6d138bc90e08bb0dbfac41173246f47
Nathan Almeida <nalmeida345@g.rwu.edu>
2020-09-22 11:52:06 -0400

    commit on 'master'

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyACRwXO54T4LkC6zsUKCIt0jHMxR-MIrt0",
+    authDomain: "comsc230-firegram.firebaseapp.com",
+    databaseURL: "https://comsc230-firegram.firebaseio.com",
+    projectId: "comsc230-firegram",
+    storageBucket: "comsc230-firegram.appspot.com",
+    messagingSenderId: "722147129642",
+    appId: "1:722147129642:web:7343a994d10a59f33f4653"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
